66a7c5124065b76c855bec1e35d4fa9fc5a42daf
nevits <zaidnevits@gmail.com>
2018-12-24 11:00:18 -0500

    Cambio en la configuracion de Mongo

--- src/main/java/com/bancodebogota/ptdo/parametria/configuration/MongoConfiguration.java
+++ src/main/java/com/bancodebogota/ptdo/parametria/configuration/MongoConfiguration.java
@@ -3,20 +3,17 @@ package com.bancodebogota.ptdo.parametria.configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 
 @Configuration
-@EnableMongoRepositories(basePackages = "com.bdg.dashboard.repository")
-@PropertySource("classpath:application.properties")
-@ComponentScan(basePackages = "com.bdg.dashboard.controller")
+//@EnableMongoRepositories(basePackages = "com.bdg.dashboard.repository")
+//@PropertySource("classpath:application.properties")
+//@ComponentScan(basePackages = "com.bdg.dashboard.controller")
 public class MongoConfiguration  extends AbstractMongoConfiguration {
 
 	private final Logger log = LoggerFactory.getLogger(MongoConfiguration.class);
